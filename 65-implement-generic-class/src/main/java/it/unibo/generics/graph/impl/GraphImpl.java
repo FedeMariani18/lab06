d55280f365110af8da712a3192d8fbcf3ac286bd
Federico Mariani - federico.mariani3@studio.unibo.it <federico.mariani3@studio.unibo.it>
2024-11-05 10:55:00 +0100

    created the file GraphImpl

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -0,0 +1,40 @@
+package main.java.it.unibo.generics.graph.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+public class GraphImpl<N> implements Graph<N>{
+
+    @Override
+    public void addNode(Object node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNode'");
+    }
+
+    @Override
+    public void addEdge(Object source, Object target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addEdge'");
+    }
+
+    @Override
+    public Set nodeSet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nodeSet'");
+    }
+
+    @Override
+    public Set linkedNodes(Object node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'linkedNodes'");
+    }
+
+    @Override
+    public List getPath(Object source, Object target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+    }
+
+}
