f47f9c297d37954556488b70c7f94e8a96973e2a
Federico Mariani - federico.mariani3@studio.unibo.it <federico.mariani3@studio.unibo.it>
2024-11-05 11:38:34 +0100

    half finished exercise n.65

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,38 +1,51 @@
 package main.java.it.unibo.generics.graph.impl;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 import it.unibo.generics.graph.api.Graph;
 
 public class GraphImpl<N> implements Graph<N>{
 
+    private final LinkedList<N> nodes = new LinkedList<>();
+    private final Map<N, LinkedList<N>> edges = new TreeMap<>();
+
     @Override
-    public void addNode(Object node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addNode'");
+    public void addNode(N node) {
+        if(nodes.contains(node) || node == null){
+            return;
+        } else {
+            nodes.add(node);
+        }
     }
 
     @Override
-    public void addEdge(Object source, Object target) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addEdge'");
+    public void addEdge(N source, N target) {
+        if(source == null || target == null || !nodes.contains(source) || !nodes.contains(target) ){
+            return;
+        }
+        
+        if(edges.containsKey(source)){
+            edges.get(source).add(target);
+        }
     }
 
     @Override
-    public Set nodeSet() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nodeSet'");
+    public Set<N> nodeSet() {
+        return new TreeSet<N>(nodes);
     }
 
     @Override
-    public Set linkedNodes(Object node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'linkedNodes'");
+    public Set<N> linkedNodes(N node) {
+        return new TreeSet<>(edges.get(node));
     }
 
     @Override
-    public List getPath(Object source, Object target) {
+    public List<N> getPath(N source, N target) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getPath'");
     }
